4d9b0b8c5d9f9a1a46e1465df43b34b8757e3666
Emily Bernstein <ebernst6@binghamton.edu>
2018-10-25 23:13:35 +0000

    seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+    events =
+        [
+        {:title => 'Game night',  :host => 'George', :date => '25-Nov-2018', :start_time =>'11:30', :end_time => '12:30', :location => "UU102", :description => "Come play games!"},
+        {:title => 'Fun time',  :host => 'Fred', :date =>'25-Nov-2018', :start_time =>'14:30', :end_time => '15:30', :location => "UU112", :description => "Fun with Fred"},
+        {:title => 'Studying CS445',  :host => 'Rose', :date =>'27-Nov-2018', :start_time =>'16:00', :end_time => '18:30', :location => 'EB Q22', :description => "Learn!"},
+        {:title => 'Hiking',  :host => 'Brett', :date =>'20-Nov-2018', :start_time =>'10:30', :end_time => '13:30', :location => 'Nature Preserve', :description => "Go hiking! Please sign up ahead of time"}
+        
+        ]
+    
+    events.each do |event|
+        Event.create! event
+    end
