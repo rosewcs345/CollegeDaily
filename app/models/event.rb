f1b08b9d96bd7c136e99cfe4e2a143a0441384e9
Rupam Roy <rroy6@binghamton.edu>
2018-12-09 06:56:41 +0000

    event date validation now considers date/time in Eastern Time Zone

--- app/models/event.rb
+++ app/models/event.rb
@@ -16,10 +16,15 @@ class Event < ActiveRecord::Base
       hour = self.start_time.hour
       min = self.start_time.min
       sec = self.start_time.sec
-       
-       
-      if DateTime.new(year, month, day, hour, min, sec) < DateTime.now.in_time_zone("Eastern Time (US & Canada)") then
-        errors.add(self.date.to_s, ": Incorrect Date value. Must be in Future.")
+      
+      eventDateTime = DateTime.now.in_time_zone("Eastern Time (US & Canada)").change( {year: year, month: month,
+        day: day, hour: hour, min: min, sec: sec })
+      
+      dateTimeNow = DateTime.now.in_time_zone("Eastern Time (US & Canada)")
+      errorMsg = self.date.to_s + " " + self.class.format_time(self.start_time)
+      
+      if eventDateTime < dateTimeNow then
+        errors.add(errorMsg, ": Incorrect Date and Time value. Must be in Future.")
       end
     end
     
