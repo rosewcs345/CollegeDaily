2a592db08cb3b61ae92cac50f30a41af53387bea
Aditya Tilyalkar <atilyal1@binghamton.edu>
2018-12-12 02:36:52 +0000

    Loading event specific fields.

--- app/assets/javascripts/form_validation.js
+++ app/assets/javascripts/form_validation.js
@@ -13,12 +13,35 @@ $(document).ready(function(){
     const [yy, mm, dd ] = ["#event_date_1i", "#event_date_2i", "#event_date_3i"];
     const [st_hr, st_mt, et_hr, et_mt] = ["#event_start_time_4i", "#event_start_time_5i", "#event_end_time_4i", "#event_end_time_5i"];
     
-    $(document).on('submit', '#new_event', function(e){    
-        if(!isForm){
-            e.preventDefault();
-            showErrors();
-        }
-    });
+    
+    function updateDate(){
+        const [ day, month, year ] = [$(dd), $(mm), $(yy)]; 
+        const date = new Date(Number(year.val()), Number(month.val()) - 1, Number(day.val()));
+        const today = new Date();
+        
+        (today > date) ? isDate = false : isDate = true;
+            
+        isForm = isFormValid();
+    }
+    
+    function updateTime(){
+        const [ day, month, year, start_hr, start_mt, end_hr, end_mt ] = [$(dd), $(mm), $(yy), $(st_hr), $(st_mt), $(et_hr), $(et_mt)]; 
+        const start = new Date(Number(year.val()), Number(month.val()) - 1, Number(day.val()), Number($(st_hr).val()), Number($(st_mt).val()), 0);
+        const end = new Date(Number(year.val()), Number(month.val()) - 1, Number(day.val()), Number($(et_hr).val()), Number($(et_mt).val()), 0);
+        
+        ( start < end ) ? isTime = true : isTime = false;
+
+        console.log(start);
+        console.log(end);
+        
+        isForm = isFormValid();
+    }
+    
+    function isValidElem($elem){
+        if($.trim($elem.val())) return true;
+        else return false;
+    }        
+    
     function showErrors(){
        (isTitle) ? removeErr(title) : showErr(title, "Title is empty");
        (isHost) ? removeErr(host) : showErr(host, "Host is empty");
@@ -27,48 +50,6 @@ $(document).ready(function(){
        (isDate) ? removeErr(dd) : showErr(dd, "Date should be future date");
        (isTime) ? removeErr(et_mt) : showErr(et_mt, "End time should be greater than start time");
     }
-        
-    $(document).on('blur', title, function(e){
-        isTitle = isValidElem($(this));
-        isForm = isFormValid();   
-    });
-    $(document).on('blur', host, function(e){
-        isHost = isValidElem($(this));
-        isForm = isTitle & isHost & isLocation & isDesc & isDate & isTime;   
-    });
-    $(document).on('blur', loc, function(e){
-        isLocation = isValidElem($(this));
-        isForm = isFormValid();;   
-    });
-    $(document).on('blur', desc, function(e){
-        isDesc = isValidElem($(this));
-        isForm = isFormValid();;   
-    });
-    $(document).on('blur', dd, function(e){
-        const [ day, month, year ] = [$(dd), $(mm), $(yy)]; 
-        const date = new Date(Number(year.val()), Number(month.val()) - 1, Number(day.val()));
-        const today = new Date();
-        
-        (today > date) ? isDate = false : isDate = true;
-            
-        isForm = isFormValid();;    
-    });
-    $(document).on('blur', et_mt, function(e){
-        const [ day, month, year, start_hr, start_mt, end_hr, end_mt ] = [$(dd), $(mm), $(yy), $(st_hr), $(st_mt), $(et_hr), $(et_mt)]; 
-        const start = new Date(Number(year.val()), Number(month.val()) - 1, Number(day.val()), Number($(st_hr).val()), Number($(st_mt).val()), 0);
-        const end = new Date(Number(year.val()), Number(month.val()) - 1, Number(day.val()), Number($(et_hr).val()), Number($(et_mt).val()), 0);
-        
-        ( start < end ) ? isTime = true : isTime = false;
-
-        isForm = isFormValid();;    
-    });
-
-    
-    function isValidElem($elem){
-        if($.trim($elem.val())) return true;
-        else return false;
-    }        
-    
     
     function showErr(id, msg){
         const elem = $(id).parent();
@@ -87,5 +68,61 @@ $(document).ready(function(){
     
     function isFormValid(){
         return isForm = isTitle & isHost & isLocation & isDesc & isDate & isTime;   
-    }
+    }        
+    
+    //event handlers
+
+    $(document).on('submit', '#new_event', function(e){    
+        if(!isForm){
+            e.preventDefault();
+            showErrors();
+        }
+    });
+    
+    $(document).on('blur', title, function(e){
+        isTitle = isValidElem($(this));
+        isForm = isFormValid();   
+    });
+    
+    $(document).on('blur', host, function(e){
+        isHost = isValidElem($(this));
+        isForm = isTitle & isHost & isLocation & isDesc & isDate & isTime;   
+    });
+    $(document).on('blur', loc, function(e){
+        isLocation = isValidElem($(this));
+        isForm = isFormValid();;   
+    });
+    $(document).on('blur', desc, function(e){
+        isDesc = isValidElem($(this));
+        isForm = isFormValid();;   
+    });
+    
+    $(document).on('blur', dd, function(e){
+        updateDate();
+    });
+
+    $(document).on('blur', mm, function(e){
+        updateDate();
+    });
+
+    $(document).on('blur', yy, function(e){
+        updateDate();
+    });
+    
+    $(document).on('blur', st_hr, function(e){
+        updateTime();
+    });
+
+    $(document).on('blur', st_mt, function(e){
+        updateTime();
+    });
+
+    $(document).on('blur', et_hr, function(e){
+        updateTime();
+    });
+
+    $(document).on('blur', et_mt, function(e){
+        updateTime();
+    });
+    
 });
